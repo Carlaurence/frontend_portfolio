34f51019b0b4ade6a251139b46de739fae2ddfd1
Carlaurence <ingeniero.indbermeo@gmail.com>
2023-10-25 10:36:46 -0500

    tercer commit: Desconexion Back y Add codigo para envio del Form al Email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import { BsTelephonePlusFill, BsFillPersonCheckFill, BsEnvelopeAtFill } from "react-icons/bs";
 import { IoLogoWhatsapp } from "react-icons/io";
@@ -8,63 +8,35 @@ import crud from '../api/crud'
 import swal from "sweetalert";
 import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
+import { useForm, ValidationError } from '@formspree/react';
 
 const Contact = () => {
+
+    /*SE DESHABILITO ESTA PARTE DE CODIGO PORQUE NO ESTAMOS UTILIZANDO CONEXION A LA API NI A LA DB 
+    const [data, setData] = useState({ name: '', email: '', subject: '', message: ''})
+    const { name, email, subject, message } = data
+    const onChange = (e) => { setData({ ...data, [e.target.name]: e.target.value.trimStart() })}
+    const createMessage = async () => {
+        const body = { name: data.name, phone: phoneNumber, subject: data.subject, email: data, email, message: data.message}
+        const response = await crud.POST(`/api/message`, body)
+        //CLEANING INPUT BOXES FILELD// setData({ name: '', subject: '', email: '', message: '' })
+        //CLEANING INPUT NUMBER PHONE FILELD// setPhoneNumber('+1')
+        //SUCCESSFULL MESSAGE// swal("success", "Thank you for contacting me. I will be in contact with you as soon as posible", "success");
+        //RESET useState[product] VALUES FOR EACH FIELD// const resetDataValues = Object.keys(data).map(key => {data[key] = ''})
+    }
+    const onSubmit = (e) => { e.preventDefault(); createMessage();}
+    */
 
     const [phoneNumber, setPhoneNumber] = useState('')
     const onChangePhoneNummber = (value) => {
         setPhoneNumber(value)
     }
-
-    const [data, setData] = useState({
-        name: '',
-        email: '',
-        subject: '',
-        message: ''
-    })
-
-    const { name, email, subject, message } = data
-    //console.log(data)
-
-    const onChange = (e) => {
-        setData({
-            ...data,
-            [e.target.name]: e.target.value.trimStart()
-        })
-    }
-
-    const createMessage = async () => {
-
-        const body = {
-            name: data.name,
-            phone: phoneNumber,
-            subject: data.subject,
-            email: data, email,
-            message: data.message
-        }
-
-        const response = await crud.POST(`/api/message`, body)
-        //console.log(response.msg)
-
-        //CLEANING INPUT BOXES FILELD
-        setData({ name: '', subject: '', email: '', message: '' })
-
-        //CLEANING INPUT NUMBER PHONE FILELD
-        setPhoneNumber('+1')
-
-        //SUCCESSFULL MESSAGE
-        swal("success", "Thank you for contacting me. I will be in contact with you as soon as posible", "success");
-
-        //RESET useState[product] VALUES FOR EACH FIELD
-        const resetDataValues = Object.keys(data).map(key => {
-            data[key] = ''
-        })
-
-    }
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        createMessage();
+    const [state, handleSubmit] = useForm("xrgwjakr");
+    const navigate = useNavigate();
+    if (state.succeeded) {
+        navigate('/')
+        swal("success", "Thank you for contacting me. I will be in contact with you as soon as posible", "success"); 
+        //<p>Thanks for joining!</p>;
     }
 
     return (
@@ -102,16 +74,17 @@ const Contact = () => {
                     </div>
 
                     {/*FORMULARIO DE CONTACTO LADO DERECHO*/}
-                    <form onSubmit={onSubmit} className=" flex flex-col gap-1 text-white text-[15px] font-semibold py-6 px-10 md:px-4 lg:px-10">
+                    <form onSubmit={handleSubmit} className=" flex flex-col gap-1 text-white text-[15px] font-semibold py-6 px-10 md:px-4 lg:px-10">
 
                         <div className="flex flex-col">
                             <label>Name</label>
                             <input className="text-black px-2 py-1 rounded-md leading-tight border-double border-4 border-black"
+                                id="name"
                                 type="text"
                                 name="name"
-                                value={name}
+                                //value={name}
                                 placeholder="Enter your name"
-                                onChange={onChange}
+                                //onChange={onChange}
                             >
 
                             </input>
@@ -129,8 +102,10 @@ const Contact = () => {
                                         required: true
 
                                     }}
-                                
                                 />
+
+                                <input className="hidden" id="phone" name="phone" value={phoneNumber}></input>
+
                             </label>
                             
                         </div>
@@ -138,25 +113,28 @@ const Contact = () => {
                         <div className="flex flex-col">
                             <label>Email</label>
                             <input className="text-black px-2 py-1 rounded-md leading-tight border-double border-4 border-black"
+                                id="email"
                                 type="email"
                                 name="email"
-                                value={email}
+                                //value={email}
                                 placeholder="Enter a valid email address"
                                 required
-                                onChange={onChange}
+                                //onChange={onChange}
                             >
-
                             </input>
+
+                            <ValidationError prefix="Email" field="email" errors={state.errors}/>
                         </div>
 
                         <div className="flex flex-col">
                             <label>Subject</label>
                             <input className="text-black px-2 py-1 rounded-md leading-tight border-double border-4 border-black"
+                                id="subject"
                                 type="text"
                                 name="subject"
-                                value={subject}
+                                //value={subject}
                                 placeholder="Subject"
-                                onChange={onChange}
+                                //onChange={onChange}
                             >
 
                             </input>
@@ -165,13 +143,16 @@ const Contact = () => {
                         <div className="flex flex-col">
                             <label>Message</label>
                             <textarea className="text-black px-2 py-1 rounded-md leading-tight border-double border-4 border-black"
+                                id="message"
                                 type="text"
                                 name="message"
-                                value={message}
+                                //value={message}
                                 rows={4}
                                 placeholder="Enter your message"
-                                onChange={onChange}
+                                //onChange={onChange}
                             ></textarea>
+
+                            <ValidationError prefix="Message" field="message" errors={state.errors}/>
                         </div>
                         {/*BOTON PARA ENVIAR LA INFORMACION DEL FORMULARIO*/}
                         <button type="submit" className="mt-2 bg-yellow-400 text-black rounded-md w-[80px] h-7 hover:opacity-70">Submit</button>
@@ -185,15 +166,3 @@ const Contact = () => {
 }
 
 export default Contact;
-
-
-/*
-<input className="text-black px-2 py-1 rounded-md leading-tight border-double border-4 border-black"
-type="number"
-name="phone"
-value={phone}
-placeholder="Enter a code area + phone number"
-onChange={onChange}
->
-</input>
-*/
